8880e37f75e74c39d383fe10c9b9f120c0d5414f
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-03-12 14:39:19 -0700

    Release AcousticMobilePush build: 3.9.19
    Release AcousticMobilePush Change Notes:
    Bug Fixes
    
    - Fixed problem saving to keychain (error -25299), when migrating from iOS 380 to 385 SDK.
    
    Improvements:
    
    - Moved to new repo location https://github.com/go-acoustic/Acoustic-Mobile-Push-iOS.
    - Add PrivacyInfo.xcprivacy and sign xcframeworks.
    - Update for xcode 15.x.
    - Add to cocoapods public site.
    - Add Carthage support.
    - Add swift packages support.
    - Inapp Plugin (Image) supports duration properly.
    
    Our environment for this release:
    
    - Xcode 15.2
    - MacOS 14.3.1
    - iOS 12.x to 17.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    	- watch
    		- arm64
    		- arm64_32
    		- armv7k
    		- i386
    		- x86_64
    	- macos
    		- arm64
    		- x86_64-maccatalyst
    		- x86_64-simulator
    
    Known Issues
    
    - AcousticMobilePushWatch.xcframework is not supported in xcode 15 and above, but it works with current AcousticMobilePush.xcframework.

--- Package.swift
+++ Package.swift
@@ -35,6 +35,6 @@ let package = Package(
         .binaryTarget(
             name: "AcousticMobilePushWatch",
             url: "https://github.com/go-acoustic/Acoustic-Mobile-Push-iOS/releases/download/3.9.19/AcousticMobilePushWatch_XCFramework_Release.zip",
-            checksum: "7880d2d0e104e1230417822a571f8b8bd67e4f338a0ccc805bb42fa58aaed003"),
+            checksum: "cab322fd3b7bf31df19d110321cbf63e9b1335e457ff93da13d4ef7ed233ce18"),
     ]
 )
